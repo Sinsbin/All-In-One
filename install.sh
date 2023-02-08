5738195db7a481a35ef14f3424006ba30582d0c3
Bin <k1234988@gmail.com>
2023-02-08 15:50:44 +0800

    scripts

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+echo "google install"
+winget install google.Chrome
+echo "VScode install"
+winget install Microsoft.VisualStudioCode
+echo "VScode Python3.9"
+winget install python3.9
+echo "VScode Git"
+winget install git.git
+echo "VScode Mysql"
+winget install Oracle.mysql
+echo "VScode mongoDB"
+winget install mongodb
+
